b870b4f5e515e5782f9d20ec961f644561bbe268
KyZoar <marion.pellicer@laposte.net>
2018-05-17 19:41:50 -0400

    Premiers pas pour les constructeurs

--- quizz/src/devoir2/question1/CMMQuizz.java
+++ quizz/src/devoir2/question1/CMMQuizz.java
@@ -6,16 +6,16 @@ public class CMMQuizz implements QuizzMaster {
     private int reponse;
     private String type;
 
-    public CMMQuizz ()
+    public CMMQuizz (String type, String question, String choix0, String choix1, String choix2, String choix3, int reponse)
     {
     	choix = new String [4];
-    	question = "3.	La méthode  CMM (Capability Maturity Model) se divise en :";
-    	choix[0] = "a)	3 niveaux";
-    	choix[1] = "b)	4 niveaux";
-    	choix[2] = "c)	5 niveaux";
-    	choix[3] = "d)	6 niveaux";
-    	reponse = 2;
-    	type="Question sur CMM";
+    	this.question = question; //"3.	La méthode  CMM (Capability Maturity Model) se divise en :";
+    	choix[0] = choix0; //"a)	3 niveaux";
+    	choix[1] = choix1; //"b)	4 niveaux";
+    	choix[2] = choix2; //"c)	5 niveaux";
+    	choix[3] = choix3; //"d)	6 niveaux";
+    	this.reponse = reponse; //2;
+    	this.type = type; //"Question sur CMM";
      }
 	@Override
 	public String getChoix(int indice) {
--- quizz/src/devoir2/question1/UMLQuizz.java
+++ quizz/src/devoir2/question1/UMLQuizz.java
@@ -6,16 +6,16 @@ public class UMLQuizz implements QuizzMaster {
     private String [] choix;
     private int reponse;
 
-    public UMLQuizz ()
+    public UMLQuizz (String type, String question, String choix0, String choix1, String choix2, String choix3, int reponse)
     {
-    	type="Question sur UML";
+    	this.type = type; //"Question sur UML";
     	choix = new String [4];
-    	question = "Parmi les diagrammes suivants, lequel a été introduit dans UML2 ?";
-    	choix[0] = "Diagramme d’activité";
-    	choix[1] = "Diagramme de Classes";
-    	choix[2] = "Diagramme de Temps";
-    	choix[3] = "Diagramme des cas d’utilisation";
-    	reponse = 2;
+    	this.question = question; //"Parmi les diagrammes suivants, lequel a été introduit dans UML2 ?";
+    	choix[0] = choix0; //"Diagramme d’activité";
+    	choix[1] = choix1; //"Diagramme de Classes";
+    	choix[2] = choix2; //"Diagramme de Temps";
+    	choix[3] = choix3; //"Diagramme des cas d’utilisation";
+    	this.reponse = reponse; //2;
      }
 
 	@Override
